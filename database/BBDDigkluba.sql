03c75c2f702f3ce93013fd4f9256a7b52897b7e5
lukaquindon08 <lukacarmona118@gmail.comgit config --global user.email lukacarmona115@gmail.com>
2022-10-14 12:06:03 +0200

    bbdd

--- database/BBDDigkluba.sql
+++ database/BBDDigkluba.sql
@@ -11,7 +11,7 @@ Create Database IGKLUBA;
     Nickname Varchar(30) NOT NULL,
     Foto varchar(15),
     Centro Varchar(20),
-    Foreing Key (Centro) references Centro(ID_Centro),
+    FOREIGN KEY  (Centro) references Centro(ID_Centro),
     Fecha_Nacimiento DATE,
     Tipo ENUM('Alumno','Profesor', 'Administrador') NOT NULL,
     Validado BOOLEAN NOT NULL,
@@ -35,47 +35,47 @@ CREATE TABLE Libros (
     NotaMedia INT(2) NOT NULL,
     Num_Lectores INT(4) NOT NULL,
     Idioma Varchar(15) NOT NULL,
-    Foreing Key (Idioma) references IdiomaLibro(Idioma)
+     FOREIGN KEY  (Idioma) references IdiomaLibro(Idioma)
 ); 
 CREATE TABLE Valoraciones (
     ID_Valoracion Varchar(20) NOT NULL PRIMARY KEY,
     Nota Varchar(30) NOT NULL,
     Edad INT(4) NOT NULL,
     Nickname Varchar(30) NOT NULL,
-    Foreing Key (Nickname) references Usuarios(Nickname),
+     FOREIGN KEY  (Nickname) references Usuarios(Nickname),
     Titulo_libro  Varchar(20) NOT NULL,
-    Foreing Key (Titulo_libro) references Libros(Titulo_libro),
+     FOREIGN KEY  (Titulo_libro) references Libros(Titulo_libro),
     Idioma Varchar(15) NOT NULL,
-    Foreing Key (Idioma) references IdiomaLibro(Idioma)
+     FOREIGN KEY  (Idioma) references IdiomaLibro(Idioma)
 );
 CREATE TABLE Opiniones (
     ID_Opinion Varchar(20) NOT NULL PRIMARY KEY, 
     Nombre Varchar(30) NOT NULL,
-    Foreing Key (Nombre) references Usuarios(Nombre),
+     FOREIGN KEY  (Nombre) references Usuarios(Nombre),
     Fecha_Nacimiento DATE,
-    Foreing Key (Fecha_Nacimiento) references Usuarios(Fecha_Nacimiento)
+     FOREIGN KEY  (Fecha_Nacimiento) references Usuarios(Fecha_Nacimiento)
 );
 
 CREATE TABLE PeticionDeLibro (
     Titulo_libro  Varchar(20) NOT NULL,
-    Foreing Key (Titulo_libro) references Libros(Titulo_libro),
+     FOREIGN KEY  (Titulo_libro) references Libros(Titulo_libro),
     ID_Peticion Varchar(20) NOT NULL PRIMARY KEY,
     Fecha_Nacimiento DATE,
-    Foreing Key (Fecha_Nacimiento) references Usuarios(Fecha_Nacimiento),
+     FOREIGN KEY  (Fecha_Nacimiento) references Usuarios(Fecha_Nacimiento),
     Estado ENUM('Aceptada','EnEspera', 'Denegada'),
-    Nickname Varchar(30) NOT NULL
-    Foreing Key (Nickname) references Usuarios(Nickname)
+    Nickname Varchar(30) NOT NULL,
+    FOREIGN KEY  (Nickname) references Usuarios(Nickname)
 );
 CREATE TABLE Clase (
     ID_Clase Varchar(20) NOT NULL PRIMARY KEY,
     FechaLimite Date NOT NULL,
     Codigo INT(4) NOT NULL, 
     Correo Varchar(30) NOT NULL,
-    Foreing Key (Correo) references Usuarios(Correo)
+     FOREIGN KEY  (Correo) references Usuarios(Correo)
 );
 CREATE TABLE ListaDeClases (
     ID_Clase Varchar(20) NOT NULL PRIMARY KEY, 
-    Foreing Key (ID_Clase) references Clase(ID_Clase)
+    FOREIGN KEY  (ID_Clase) references Clase(ID_Clase),
     Nombre Varchar(30) NOT NULL,
-    Foreing Key (Nombre) references Usuarios(Nombre)
+    FOREIGN KEY (Nombre) references Usuarios(Nombre)
 );
