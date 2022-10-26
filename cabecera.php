67e1aa19903c5c709d1750966b9ade787b458e2a
lukaquindon08 <lukacarmona118@gmail.comgit config --global user.email lukacarmona115@gmail.com>
2022-10-26 10:18:26 +0200

    si

--- cabecera.php
+++ cabecera.php
@@ -1,39 +1,39 @@
 <header id="cabecera">
-       
-       <div id="caja-logo-titulo">
-              <figure id="logo">
-                   <img src="img/logo.jpg" alt="logo">
-               </figure>
 
-               <h1>IGKLUBA</h1>
-       </div>
-       
-        <div id="caja-botones">
-              
-               <div id="btn-buscador" class="btn">
-                <i class="fas fa-search"></i>
-               </div>
-           <div id="buscador" class="ocultar">
-               <input type="text" id="buscador-texto" placeholder="Bilatu" size="20">
-               <i class="fas fa-search"></i>
-           </div>
-      
-           <div id="btn-nav" class="btn">
-                   <span class="linea"></span>
-                   <span class="linea"></span>
-                   <span class="linea"></span>
-            </div>
+    <div id="caja-logo-titulo">
+        <figure id="logo">
+            <img src="img/logo.jpg" alt="logo">
+        </figure>
+
+        <h1>IGKLUBA</h1>
+    </div>
+
+    <div id="caja-botones">
+
+        <div id="btn-buscador" class="btn">
+            <i class="fas fa-search"></i>
         </div>
-        
-        <nav id="menu-nav" class="ocultar">
-            <ul>
-                <li><a href=""><i class="fas fa-chalkboard-teacher"></i>Nire klaseak</a></li>
-                <li><a href="mis_libros.php"><i class="fa fa-book-open"></i>Nire liburuak</a></li>
-                <li><a href=""><i class="fas fa-user"></i>Nire datuak</a></li>
-                <li><a href="nuevo_profesor.php"><i class="	far fa-id-badge"></i>Nuevo profesor</a></li>
-                <li><a href="validacion.php"><i class="fas fa-check"></i>Balioztatzeko</a></li>
-                <li><a href="logout.php"><i class="fas fa-times"></i>Saioa itxi</a></li>
-           </ul>
-       </nav>
-      
-    </header>
+        <div id="buscador" class="ocultar">
+            <input type="text" id="buscador-texto" placeholder="Bilatu" size="20">
+            <i class="fas fa-search"></i>
+        </div>
+
+        <div id="btn-nav" class="btn">
+            <span class="linea"></span>
+            <span class="linea"></span>
+            <span class="linea"></span>
+        </div>
+    </div>
+
+    <nav id="menu-nav" class="ocultar">
+        <ul>
+            <li><a href=""><i class="fas fa-chalkboard-teacher"></i>Nire klaseak</a></li>
+            <li><a href="mis_libros.php"><i class="fa fa-book-open"></i>Nire liburuak</a></li>
+            <li><a href="perfilpersonal.php"><i class="fas fa-user"></i>Nire datuak</a></li>
+            <li><a href="nuevo_profesor.php"><i class="	far fa-id-badge"></i>Nuevo profesor</a></li>
+            <li><a href="validacion.php"><i class="fas fa-check"></i>Balioztatzeko</a></li>
+            <li><a href="logout.php"><i class="fas fa-times"></i>Saioa itxi</a></li>
+        </ul>
+    </nav>
+
+</header>
