e3f2c0981912c3c34a93121850ee6729fa85e359
lukaquindon08 <lukacarmona118@gmail.comgit config --global user.email lukacarmona115@gmail.com>
2022-10-28 09:06:12 +0200

    si

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<?php include('database/conexion.php');?>
-<?php 
-    // Comprobamos si existe la sesión de apodo
-    session_start();
-    if (!isset($_SESSION['nickname'])) {
-        // En caso contrario devolvemos a la página login.php
-        header('Location: login.php');
-        die();
-    }
-?>    
+<?php include('database/conexion.php'); ?>
+<?php
+// Comprobamos si existe la sesión de apodo
+session_start();
+if (!isset($_SESSION['nickname'])) {
+    // En caso contrario devolvemos a la página login.php
+    header('Location: login.php');
+    die();
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -21,7 +21,7 @@
 
     <!-- Custom Styles -->
     <link rel="stylesheet" href="css/estilos.css">
-    
+
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
@@ -30,61 +30,50 @@
 </head>
 
 <body>
-    
+
     <?php include('cabecera.php'); ?>
+    <?php include('funcionLibros.php'); ?>
 
     <main id="contenido">
-        
-            <?php 
-        
-            // Prepara SELECT
-            $consulta = $miPDO->prepare('SELECT * FROM libros;');
+
+    <?php
+    $busqueda = isset($_REQUEST['busqueda']) ? $_REQUEST['busqueda'] : null;
     
-            // Ejecuta consulta
-            $consulta->execute();
+    if($busqueda === '' || $busqueda === null){
 
-                $respuesta = $consulta ->fetchAll();
-               // foreach($respuesta as $posicion =>$libros):   
-     
-                    foreach($respuesta as $posicion =>$libros){
-                        
-                   //$titulo-libro = $libros['titulo_libro'];
-                        
-                    echo "<div id='libro'>";
-                    
-                        //Imagen
-                        echo "<figure class='img-libro'><img src='img/" . $libros['foto'] . "'></figure>";
-                    
-                            
-                        echo "<div class='caja-info-libro'>";
-                        //Titulo
-                        echo "<p class='libro-titulo' method='get'>" . $libros['titulo_libro'] . "</p>";
-                       
-                    
-                        //Autor
-                        echo "<p class='libro-autor'>" . $libros['autor'] . "</p>";
-                        
-                        //Valoración
-                        echo "<div class='caja-notamedia'>";
-                        echo "<p class='libro-notamedia-text'>Batez besteko nota</p>";
-                        echo "<p class='libro-notamedia'>" . $libros['notamedia'] . "</p>";
-                        echo "</div>";                        
-                        //Edad media
-                        echo "<p class='libro-edad-media'>Batez besteko adina: " . $libros['edadmedia'] . "</p>";
+        $consulta = $miPDO->prepare('SELECT * FROM libros;');
 
-                        //Enlace a la ficha
-                        echo "<p class='enlace-ficha'><a href='fichalibro.php?liburua=" . $libros['id_libro'] . "'>Fitxa ikusi</a></p>";
-                        echo "</div>";
-                    echo "</div>";
-                }
-                    
-                  ?>  
+        // Ejecuta consulta
+        $consulta->execute();
 
+        $respuesta = $consulta->fetchAll();
+        // foreach($respuesta as $posicion =>$libros): 
+        anadirlibros($respuesta);
         
+        }else{
+       // Variables del formulario
+        $respuesta = $miPDO->prepare("SELECT * FROM libros WHERE autor LIKE '%$busqueda%' OR titulo_libro LIKE  '%$busqueda%'");
+        $respuesta->execute();
+        $respuesta = $respuesta->fetchAll();
+        // Prepara SELECT
+        $consulta = $miPDO->prepare('SELECT * FROM libros;');
+
+        // Ejecuta consulta
+        $consulta->execute();
+
+        $respuesta = $consulta->fetchAll();
+        // foreach($respuesta as $posicion =>$libros): 
+        anadirlibros($respuesta);
+
+    }
+ 
+    ?>
+
+
     </main>
-    
+
     <?php include('pie-pagina.php'); ?>
-   
+
 </body>
 
 </html>
