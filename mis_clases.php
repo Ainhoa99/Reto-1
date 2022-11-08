ee04427dbdce58969f58ed560b41789fbe918bbc
Ainhoa Lopez <ainhoalopez99.al@gmail.com>
2022-11-08 08:26:53 +0100

    .

--- mis_clases.php
+++ mis_clases.php
@@ -15,13 +15,17 @@ if (!isset($_SESSION['nickname'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
-
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Custom Scripts -->
+    <script src="js/scripts.js"></script>
     <title>Mis Clases</title>
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 
 <body>
     <?php include('cabecera.php'); ?>
+
     <main id="contenido-mislibros">
 
         <div class="mis-libros">
