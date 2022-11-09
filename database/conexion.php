45249f265d8293de812c94e96faf2a747bd2bd3d
Ainhoa99 <ainhoalopez99.al@gmail.com>
2022-11-09 22:47:33 +0100

    .

--- database/conexion.php
+++ database/conexion.php
@@ -1,13 +1,10 @@
 <?php
-    //Variables
-    $hostDB = '127.0.0.1';
-    $nombreDB = 'igkluba';
-    $usuarioDB = 'root';
-    $contrasenyaDB = '';
+//Variables
+$hostDB = '127.0.0.1';
+$nombreDB = 'igkluba';
+$usuarioDB = 'root';
+$contrasenyaDB = '';
 
-    //Conecta con base de datos
-    $hostPDO = "mysql:host=$hostDB;dbname=$nombreDB;";
-    $miPDO = new PDO($hostPDO, $usuarioDB, $contrasenyaDB);
-
-   
-?>
+//Conecta con base de datos
+$hostPDO = "mysql:host=$hostDB;dbname=$nombreDB;";
+$miPDO = new PDO($hostPDO, $usuarioDB, $contrasenyaDB);
--- login.php
+++ login.php
@@ -1,5 +1,15 @@
 <?php
-include_once "database/conexion.php";
+//Variables
+$hostDB = 'localhost';
+$nombreDB = 'igkluba';
+$usuarioDB = 'root';
+$contrasenyaDB = 'admin';
+
+//Conecta con base de datos
+$hostPDO = "mysql:host=$hostDB;dbname=$nombreDB;";
+$miPDO = new PDO($hostPDO, $usuarioDB, $contrasenyaDB);
+
+
 ?>
 
 
