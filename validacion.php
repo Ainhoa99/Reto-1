7a20ae5fc04e400ae74e85f51e9ff3d0d60391fb
lukaquindon08 <lukacarmona118@gmail.comgit config --global user.email lukacarmona115@gmail.com>
2022-11-09 12:29:56 +0100

    validar libros

--- validacion.php
+++ validacion.php
@@ -16,23 +16,24 @@ if (!isset($_SESSION['nickname'])) {
 <html lang="en">
 
 <head>
-<head>
-    <meta charset="UTF-8">
-    <title>Balioztatzeko</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <head>
+        <meta charset="UTF-8">
+        <title>Balioztatzeko</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Custom Styles -->
-    <link rel="stylesheet" href="css/estilos.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- Custom Styles -->
+        <link rel="stylesheet" href="css/estilos.css">
 
-    <!-- Custom Scripts -->
-    <script src="js/scripts.js"></script>
-</head>
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <!-- Custom Scripts -->
+        <script src="js/scripts.js"></script>
+    </head>
 
 <body>
     <?php include('cabecera.php'); ?>
@@ -52,6 +53,7 @@ if (!isset($_SESSION['nickname'])) {
                 <?php
                 $validar = (int) isset($_REQUEST['validado']) ? $_REQUEST['validado'] : null;
                 $correo = isset($_REQUEST['correo']) ? $_REQUEST['correo'] : null;
+
                 if ($validar == 1) {
                     // Prepara UPDATE
                     $miUpdate = $miPDO->prepare('UPDATE usuarios SET VALIDADO  = 1 WHERE correo = :correo');
@@ -61,6 +63,7 @@ if (!isset($_SESSION['nickname'])) {
                             'correo' => $correo,
                         ]
                     );
+                    // Prepara UPDATE
                     // Redireccionamos a Leer
                 } else {
                     // Prepara UPDATE
@@ -80,19 +83,70 @@ if (!isset($_SESSION['nickname'])) {
                 foreach ($usuarios as $posicion => $usuario) {
                     $correo = $usuario['correo'];
                     echo "<tr>";
-                        echo "<td>" . $usuario['nombre'] . "</td>";
-                        echo "<td>" . $usuario['apellidos'] . "</td>";
-                        echo "<td>" . $usuario['nickname'] . "</td>";
-                        echo "<td class='correo-usu' name= 'correo'>" . $usuario['correo'] . "</td>";
-                        echo "<td>";
-                            echo "<a class='boton_validacion validar-si' name='check' href='validacion.php?validado=1&correo=" . $usuario['correo'] . "' ><i class='fas fa-check-circle'></i></a> ";
-                            echo "<a class='boton_validacion validar-no' name='check' href='validacion.php?validado=0&correo=" . $usuario['correo'] . "' ><i class='fas fa-times-circle'></i></a> ";
-                        echo "</td>";
+                    echo "<td>" . $usuario['nombre'] . "</td>";
+                    echo "<td>" . $usuario['apellidos'] . "</td>";
+                    echo "<td>" . $usuario['nickname'] . "</td>";
+                    echo "<td class='correo-usu' name= 'correo'>" . $usuario['correo'] . "</td>";
+                    echo "<td>";
+                    echo "<a class='boton_validacion validar-si' name='check' href='validacion.php?validado=1&correo=" . $usuario['correo'] . "' ><i class='fas fa-check-circle'></i></a> ";
+                    echo "<a class='boton_validacion validar-no' name='check' href='validacion.php?validado=0&correo=" . $usuario['correo'] . "' ><i class='fas fa-times-circle'></i></a> ";
+                    echo "</td>";
                     echo "</tr>";
                 }
-
                 ?>
-            </table>
+                <table class="tabla-validar">
+                    <tr class="definicion-usuario">
+                        <th>Azala</th>
+                        <th>Izenburua</th>
+                        <th>Idazlea</th>
+                        <th>Sinopsia</th>
+                        <th>ISBN</th>
+                        <th>Onartu?</th>
+                    </tr>
+
+                    <?php
+                    $isbn = isset($_REQUEST['isbn']) ? $_REQUEST['isbn'] : null;
+                    $validar = (int) isset($_REQUEST['validado']) ? $_REQUEST['validado'] : null;
+                    if ($validar == 1) {
+                        // Prepara UPDATE
+                        $miUpdate = $miPDO->prepare('UPDATE libros SET VALIDADO  = 1 WHERE isbn = :isbn');
+                        // Ejecuta UPDATE con los datos
+                        $miUpdate->execute(
+                            [
+                                'isbn' => $isbn,
+                            ]
+                        );
+                        // Redireccionamos a Leer
+                    } else {
+                        $miDelete = $miPDO->prepare('DELETE FROM libros WHERE isbn = :isbn');
+                        // Ejecuta UPDATE con los datos
+                        $miDelete->execute(
+                            [
+                                'isbn' => $isbn,
+                            ]
+                        );
+                        //Consulta
+                    }
+                    $consulta = $miPDO->prepare("SELECT foto, titulo_libro, autor, sinopsis, isbn FROM libros WHERE validado=0");
+                    $consulta->execute();
+                    $usuarios = $consulta->fetchAll();
+                    $miUpdate = '';
+                    foreach ($usuarios as $posicion => $usuario) {
+                        $isbn = $usuario['isbn'];
+                        echo "<tr>";
+                        echo "<td> <figure class='img-libro'><img src='src/" . $usuario['foto'] . "'></figure> </td>";
+                        echo "<td>" . $usuario['titulo_libro'] . "</td>";
+                        echo "<td>" . $usuario['autor'] . "</td>";
+                        echo "<td>" . $usuario['sinopsis'] . "</td>";
+                        echo "<td class='isbn-libro' name= 'isbn'>" . $usuario['isbn'] . "</td>";
+                        echo "<td>";
+                        echo "<a class='boton_validacion validar-si' name='check' href='validacion.php?validado=1&isbn=" . $usuario['isbn'] . "' ><i class='fas fa-check-circle'></i></a> ";
+                        echo "<a class='boton_validacion validar-no' name='check' href='validacion.php?validado=0&isbn=" . $usuario['isbn'] . "' ><i class='fas fa-times-circle'></i></a> ";
+                        echo "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </table>
         </form>
     </main>
     <?php include('pie-pagina.php'); ?>
