03ac357130bfbb00daca3f571564f58d184c5e18
lukaquindon08 <lukacarmona118@gmail.comgit config --global user.email lukacarmona115@gmail.com>
2022-11-10 14:22:01 +0100

    comentarios

--- formopiniones.php
+++ formopiniones.php
@@ -1,7 +1,9 @@
 <?php
+// Incluimos la conexion con la base de datos
 include('database/conexion.php');
 session_start();
 
+// Cuando el nombre no esta vacio hacemos el insert de opiniones
 if ($_GET['opinion'] != "") {
     $consulta = $miPDO->prepare('INSERT INTO opiniones (nickname , opinion, validado, id_libro)
                     VALUES (:nickname, :opinion, :validado, :id_libro)');
@@ -15,6 +17,7 @@ if ($_GET['opinion'] != "") {
             ]
         );
     } else {
+        // Cuando el nombre esta vacio hacemos el insert de todas las opiniones
         $consulta->execute(
             [
                 'nickname' => $_SESSION['nickname'],
@@ -25,4 +28,5 @@ if ($_GET['opinion'] != "") {
         );
     }
 }
+//llevamos al usuario a el apartado de ficha de libor opinada
 header('Location: fichalibro.php?liburua=' . $_GET['libro']);
